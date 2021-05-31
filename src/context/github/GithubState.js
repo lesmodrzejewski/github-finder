bab9db64ebe915993167eecd8f69cae0c7c221de
Leszek Modrzejewski <leszek.modrzejewski@gmail.com>
2021-05-31 14:59:51 +0100

    client_id & client_secret

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -4,6 +4,17 @@ import GithubReducer from './githubReducer';
 import axios from 'axios';
 import { SEARCH_USERS, GET_USER, CLEAR_USERS, GET_REPOS, SET_LOADING } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientId = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
 
     const initialState = {
@@ -19,7 +30,7 @@ const GithubState = props => {
 
         setLoading();
 
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     
         dispatch({
             type: SEARCH_USERS,
@@ -32,7 +43,7 @@ const GithubState = props => {
         setLoading();
 
         const res = await axios.get(
-            `https://api.github.com/users/${username}`
+            `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
           );
         
         dispatch({
@@ -46,7 +57,7 @@ const GithubState = props => {
         setLoading();
         
         const res = await axios.get(
-            `https://api.github.com/users/${username}/repos?per_page=9&sort=created:asc`
+            `https://api.github.com/users/${username}/repos?per_page=9&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
         )
 
         dispatch({
